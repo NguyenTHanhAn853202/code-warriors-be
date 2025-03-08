8655ddf5ea001445c32f7c503b6d1fba49726224
Nhat2510 <nhatvovantuan2510@gmail.com>
2025-03-08 17:42:47 +0700

    contest_controller

---
+++ src/routes/contests.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import contestController from "../controller/contests/contests.controller";
+
+const router = Router();
+
+router.post("/createContest", contestController.CreateContest);
+router.get("/viewAllContest", contestController.ViewAllContests);
+router.patch("/updateContest/:id", contestController.UpdateContest);
+router.delete("/deleteContest/:id", contestController.DeleteContest);
+
+export default router;
