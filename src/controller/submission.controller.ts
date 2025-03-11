b1bea04504cfe121a35636b41aa58da4ceb1b49d
NguyenTHanhAn853202 <nguyenthanhanqt@gmail.com>
2025-03-11 12:25:43 +0700

    run code feature

--- src/controller/submission.controller.ts
+++ src/controller/submission.controller.ts
@@ -43,7 +43,8 @@ class SubmissionController{
             const result  = await judge0.json()
             
             if(result.status?.id !== Judge0Status.Accepted){
-                throw new AppError(result?.stderr || result.status.description,httpCode.OK,"warning",{
+                
+                throw new AppError(result?.stderr || result?.status?.description || result?.error,httpCode.OK,"warning",{
                     testcase:testcases[i],
                     result:result
                 })
@@ -55,13 +56,66 @@ class SubmissionController{
             evaluate.memory += result.memory
             
         }
-        const userId = "67c515ad7893bdb73c6a1371"
+        const userId = req.user._id
         
         const leaderboard = await Leaderboard.findOneAndUpdate({user:userId, problem:problemId},{$inc:{attempts:1},score:evaluate.point, time:evaluate.time, memory:evaluate.memory, oldSource:sourceCode,languageId:languageId},{new:true,upsert:true})
 
         await userModel.updateOne({_id:userId},{$inc:{xp:1}})
 
-        sendResponse(res,"success","check ", httpCode.OK, {leaderboard})
+        sendResponse(res,"success","check ", httpCode.OK, leaderboard)
+    })
+
+
+    run = expressAsyncHandler(async(req:Request,res:Response)=>{
+        const {sourceCode=null,languageId=null,problemId=null} = req.body
+        if(!sourceCode || !languageId || !problemId)
+            throw new AppError("Miss information",httpCode.BAD_REQUEST,"warning")
+
+        const testcases = await testcaseModel.find({problem:problemId}).select("expectedOutput input problem")
+        const problem = await problemModel.findById(problemId)
+
+        if(!problem){
+            throw new AppError("Not found problem",httpCode.BAD_REQUEST,"warning")
+        }
+
+        let evaluate = {
+            point:0,
+            time:0,
+            memory:0,
+            score:0
+        } 
+        
+        for(let i=0; i<testcases.length; i++){
+            const judge0 = await fetch(`${URL_JUDGE0}/submissions?base64_encoded=false&wait=true`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    language_id:languageId,
+                    source_code: sourceCode,
+                    stdin: testcases[i].input,
+                    expected_output:testcases[i].expectedOutput
+                })
+            })
+            const result  = await judge0.json()
+            
+            if(result.status?.id !== Judge0Status.Accepted){
+                
+                throw new AppError(result?.stderr || result?.status?.description || result?.error,httpCode.OK,"warning",{
+                    testcase:testcases[i],
+                    result:result
+                })
+            }
+            if(result.time*1000 <= problem?.timeout){
+                evaluate.point++;
+            }
+            evaluate.time += result.time*1000
+            evaluate.memory += result.memory
+            
+        }
+        evaluate.score = evaluate.point
+        sendResponse(res,"success","done",httpCode.OK,evaluate)
     })
 
 }
--- src/routes/submission.routes.ts
+++ src/routes/submission.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import submissionController from "../controller/submission.controller";
+import { auth } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/",submissionController.evaluation)
+router.post("/",auth,submissionController.evaluation)
+router.post("/run",submissionController.run)
 
 export default router
