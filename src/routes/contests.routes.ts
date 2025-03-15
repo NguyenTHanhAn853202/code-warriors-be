ce0fb93744a29cfedf7e19b46db8de8f56896c76
thinh232 <caothinh2k03@gmail.com>
2025-03-15 21:34:37 +0700

    update api view contest v1

--- src/routes/contests.routes.ts
+++ src/routes/contests.routes.ts
@@ -5,7 +5,9 @@ import { auth } from "../middleware/auth";
 const router = Router();
 
 router.post("/createContest", auth, contestController.createContest);
-router.get("/viewAllContest", contestController.viewAllContests);
+router.get("/viewAllContest", contestController.ViewAllContests);
+router.get("/FeaturedContest", contestController.GetLatestContests);
+router.get("/my", auth, contestController.getMyContests);
 router.patch("/updateContest/:id", contestController.updateContest);
 router.delete("/deleteContest/:id", contestController.deleteContest);
 router.get("/viewDetailContest/:id", contestController.viewContestDetail);
