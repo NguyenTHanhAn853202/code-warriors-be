5873e8387d00ed7d89e2636de0ba6f8e7958018c
Nhat2510 <nhatvovantuan2510@gmail.com>
2025-03-15 16:01:19 +0700

    be_contest_v1

--- src/routes/contests.routes.ts
+++ src/routes/contests.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import contestController from "../controller/contests/contests.controller";
+import contestController from "../controller/contest.controller";
+import { auth } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/createContest", contestController.CreateContest);
-router.get("/viewAllContest", contestController.ViewAllContests);
-router.patch("/updateContest/:id", contestController.UpdateContest);
-router.delete("/deleteContest/:id", contestController.DeleteContest);
-router.get("/FeaturedContest", contestController.GetLatestContests);
+router.post("/createContest", auth, contestController.createContest);
+router.get("/viewAllContest", contestController.viewAllContests);
+router.patch("/updateContest/:id", contestController.updateContest);
+router.delete("/deleteContest/:id", contestController.deleteContest);
+router.get("/viewDetailContest/:id", contestController.viewContestDetail);
 
 export default router;
