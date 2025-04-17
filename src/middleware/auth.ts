47bda2c8f5afb00ae0cd54eadc4916f394b76652
NguyenTHanhAn853202 <nguyenthanhanqt@gmail.com>
2025-04-17 22:59:37 +0700

    auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,11 @@
 import expressAsyncHandler from "express-async-handler";
 import { NextFunction, Request, Response } from "express";
 import { ObjectId } from "mongoose";
+import jwt from 'jsonwebtoken'
+import { TOKEN_KEY } from "../utils/secret";
+import { AppError } from "../utils/AppError";
+import { httpCode } from "../utils/httpCode";
+import userModel from "../model/user.model";
 
 
 type User = {
@@ -17,13 +22,27 @@ declare module 'express-serve-static-core' {
 }
 
 export const auth = expressAsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-    const user = {
-        _id:"67ea6414a85677ccfd64c1b1",
-        username:"",
-        role:''
-    }
-    req.user = user
+    const token = req.cookies["token"]
+    const decoded = jwt.verify(token,TOKEN_KEY)
+    if(!decoded)
+      throw new AppError("token was expired",httpCode.UNAUTHORIZED,"error")
+    req.user = decoded as User
     next()
+})
+
+
+/*
+  use after authen, admin and auth function are always go together
+  example: router.post('/abc,auth,admin,callback function)
+*/
+export const admin = expressAsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+  const userId = req.user._id
+  const user = await userModel.findById(userId)
+  if(!user)
+    throw new AppError("Dont find this user",httpCode.UNAUTHORIZED,"error")
+  if(user.role !== 'admin')
+    throw new AppError("You are not admin",httpCode.UNAUTHORIZED,"error")
+  next()
 })
 
 export const authSocket = expressAsyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
