def7fc7b086e2945ef2f7d9b314589c1badce05e
NguyenTHanhAn853202 <nguyenthanhanqt@gmail.com>
2025-05-05 21:54:59 +0700

    123

--- src/routes/room.routes.ts
+++ src/routes/room.routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 import RoomBattleController from "../controller/room/room.controller";
-import { auth } from "@/middleware/auth";
+import { auth } from "../middleware/auth";
 
 router.get("/", RoomBattleController.getRooms);
 
