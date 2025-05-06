5ab7a2f8188d2047540e2a16787feb8e5d42b533
Nhat2510 <nhatvovantuan2510@gmail.com>
2025-05-06 10:43:50 +0700

    api rank of contest

--- src/routes/submission.routes.ts
+++ src/routes/submission.routes.ts
@@ -4,8 +4,9 @@ import { auth } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/",auth,submissionController.evaluation)
-router.post("/run",submissionController.run)
-router.get("/history/:problemId",auth,submissionController.historySubmit)
+router.post("/", auth, submissionController.evaluation);
+router.post("/run", submissionController.run);
+router.get("/history/:problemId", auth, submissionController.historySubmit);
+router.get("/top3/:problemId", submissionController.getTop3Leaderboard);
 
-export default router
+export default router;
