415a9e3733c14b4ee92d64cf0e4ef80fea2a2217
nhnam2003 <hoainambld2003@gmail.com>
2025-05-12 18:22:52 +0700

    error socket submit_room

--- src/model/submission.model.ts
+++ src/model/submission.model.ts
@@ -5,6 +5,7 @@ export interface ISubmission extends Document {
     user: mongoose.Types.ObjectId;
     problem: mongoose.Types.ObjectId;
     match: mongoose.Types.ObjectId | null; // Nếu là nộp bài trong trận đấu
+    room : mongoose.Types.ObjectId | null; 
     code: string;
     language: string;
     status: "Pending" | "Accepted" | "Wrong Answer" | "Runtime Error";
@@ -21,6 +22,7 @@ const submissionSchema = new Schema<ISubmission>(
       user: { type: Schema.Types.ObjectId, ref: "User", required: true },
       problem: { type: Schema.Types.ObjectId, ref: "Problem", required: true },
       match: { type: Schema.Types.ObjectId, ref: "Match", default: null }, // Bổ sung matchId
+      room : { type: Schema.Types.ObjectId, ref: "RoomBattle", default: null },
       code: { type: String, required: true },
       language: { type: String, required: true },
       status: { type: String, enum: ["Pending", "Accepted", "Wrong Answer", "Runtime Error"], default: "Pending" },
